26d1e93d2ca9042e127f61e93c5c1fa66a0a0b3c
EvgeniiaKhokhlova <evkhokhlova95@gmail.com>
2020-09-22 23:45:05 +0300

    branch1

---
+++ units_converter.py
@@ -0,0 +1,8 @@
+time = int(input("Enter the time in seconds: "))
+
+Hour = time // 3600
+Hour1 = time % 3600
+Minute = Hour1 // 60
+Second = Hour1 % 60
+
+print(Hour, "Hour", Minute, "Minutes", Second, "Seconds")
